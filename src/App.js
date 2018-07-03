dc88434d0277019f685b012178ebe89cbe865421
Heather You <heatherhyou@gmail.com>
2018-07-03 14:42:35 -0400

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 
-import Bookshelf from './Bookshelf';
-import Home from './Home';
-import Search from './Search';
+import Bookshelf from './Bookshelf'
+import Home from './Home'
+import Search from './Search'
 
 class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        <Search />
-        <Home />
+        <Route exact path='/' component={Home} />
+        <Route path='/search' component={Search} />
       </div>
     )
   }
