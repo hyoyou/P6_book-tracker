e8fe8e67298bdd877a88828f030e7349cc4b9221
Heather You <heatherhyou@gmail.com>
2018-07-03 14:47:59 -0400

    Replace anchor tags with Link

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import Bookshelf from './Bookshelf';
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import Bookshelf from './Bookshelf'
 
 export default class Home extends Component {
   render() {
@@ -120,7 +121,7 @@ export default class Home extends Component {
           </div>
         </div>
         <div className="open-search">
-          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+          <Link to='/search'>Add a book</Link>
         </div>
       </div>
     )
