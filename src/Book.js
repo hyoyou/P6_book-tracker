9247eabf637e6fb615202b50645b23f99e73c23d
Heather You <heatherhyou@gmail.com>
2018-07-06 10:40:18 -0400

    refactor to functional component

--- src/Book.js
+++ src/Book.js
@@ -1,30 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class Book extends Component {
-  render() {
-    const { book } = this.props;
-
-    return (
-      <div className="book">
-        <div className="book-top">
-          <div className="book-cover" style={{
-            width: 128,
-            height: 193,
-            backgroundImage: `url("${book.imageLinks ? book.imageLinks.thumbnail : 'https://imgur.com/ZHDkYoR.jpg'}")`
-          }}></div>
-          <div className="book-shelf-changer">
-            <select value={book.shelf ? book.shelf : "none"} onChange={event => this.props.moveBook(book, event.target.value)}>
-              <option value="move" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
+const Book = ({ book, moveBook }) => {
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div className="book-cover" style={{
+          width: 128,
+          height: 193,
+          backgroundImage: `url("${book.imageLinks ? book.imageLinks.thumbnail : 'https://imgur.com/ZHDkYoR.jpg'}")`
+        }}></div>
+        <div className="book-shelf-changer">
+          <select value={book.shelf ? book.shelf : "none"} onChange={event => moveBook(book, event.target.value)}>
+            <option value="move" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
         </div>
-        <div className="book-title">{book.title}</div>
-        <div className="book-authors">{book.authors && book.authors.join(', ')}</div>
       </div>
-    )
-  }
+      <div className="book-title">{book.title}</div>
+      <div className="book-authors">{book.authors && book.authors.join(', ')}</div>
+    </div>
+  )
 }
+
+export default Book;
