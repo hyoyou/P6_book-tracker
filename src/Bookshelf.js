d63956e791d11c6745fe73569ff7065bdca67ff9
Heather You <heatherhyou@gmail.com>
2018-07-06 10:38:33 -0400

    refactor to functional component

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,44 +1,42 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import Book from './Book'
 
 const shelves = ['currentlyReading', 'wantToRead', 'read'];
 
-class Bookshelf extends Component {
-  render() {
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        {shelves.map(shelf => {
-          return (
-            <div className="list-books-content" key={shelf}>
-              <div className="bookshelf">
-                <h2 className="bookshelf-title">
-                  {shelf === 'currentlyReading' && 'Currently Reading'}
-                  {shelf === 'wantToRead' && 'Want To Read'}
-                  {shelf === 'read' && 'Read'}
-                </h2>
-                <div className="bookshelf-books">
-                  <ol className="books-grid">
-                    {this.props.books.map(book => {
-                      if (book.shelf === shelf) {
-                        return <li key={book.id}><Book book={book} moveBook={this.props.moveBook}/></li>
-                      }
-                    })}
-                  </ol>
-                </div>
+const Bookshelf = ({ books, moveBook }) => {
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      {shelves.map(shelf => {
+        return (
+          <div className="list-books-content" key={shelf}>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">
+                {shelf === 'currentlyReading' && 'Currently Reading'}
+                {shelf === 'wantToRead' && 'Want To Read'}
+                {shelf === 'read' && 'Read'}
+              </h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  {books.map(book => {
+                    if (book.shelf === shelf) {
+                      return <li key={book.id}><Book book={book} shelf={shelf} moveBook={moveBook}/></li>
+                    }
+                  })}
+                </ol>
               </div>
             </div>
-          )
-        })}
-        <div className="open-search">
-          <Link to='/search'>Add a book</Link>
-        </div>
+          </div>
+        )
+      })}
+      <div className="open-search">
+        <Link to='/search'>Add a book</Link>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Bookshelf;
